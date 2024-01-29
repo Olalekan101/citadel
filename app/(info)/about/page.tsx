4a0781a9621a37215da16d48c6415ef4d34b6346
Olalekan101 <freaksionalstudio@gmail.com>
2024-01-29 11:43:31 +0100

    fix two

--- app/(info)/about/page.tsx
+++ app/(info)/about/page.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import { getGoogleSheetsData } from "../../../dbconnection/gsheet";
-import google from "@googleapis/sheets";
 
 export default async function AboutUs() {
-  const range = `main!A2:A`;
-  const posts = await getGoogleSheetsData(range);
-  console.log(posts);
-
   return <div>About Us</div>;
 }
